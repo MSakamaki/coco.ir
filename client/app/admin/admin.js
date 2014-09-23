e5f9b6c0f33340c14e4fdbe893ef28c582875878
Fumio SAGAWA <fumio.sagawa@gmail.com>
2014-09-23 15:44:52 +0900

    angular-fullstackへ変更完了

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cocoirApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
